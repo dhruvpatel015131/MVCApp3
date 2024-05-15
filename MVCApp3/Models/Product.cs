e21ce59e64f61dd8940a439f5cffc668ce223878
dhruvpatel015131 <140480404+dhruvpatel015131@users.noreply.github.com>
2024-05-15 12:38:24 +0530

    Add project files.

---
+++ MVCApp3/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCApp3.Models
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ MVCApp3/Models/ShoppingCart.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCApp3.Models
+{
+    public class ShoppingCart
+    {
+        public List<Product> Products { get; set; }
+    }
+}
